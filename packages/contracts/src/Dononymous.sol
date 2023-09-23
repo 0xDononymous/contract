288dcf0637240e1b60e1f2a85919552b011edc5a
junochiu <ching.lun0216@gmail.com>
2023-09-22 23:13:56 -0400

    WIP provideCrumble

--- packages/contracts/src/Dononymous.sol
+++ packages/contracts/src/Dononymous.sol
@@ -8,14 +8,18 @@ import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.s
 import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/types/PoolId.sol";
 import {BalanceDelta} from "@uniswap/v4-core/contracts/types/BalanceDelta.sol";
+import {Currency, CurrencyLibrary} from "@uniswap/v4-core/contracts/types/Currency.sol";
 
 import {ERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract Dononymous is BaseHook, ERC1155 {
     using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
 
     // mapping organization => share of fee
     mapping(address organization => uint256 share) organizationShare;
+    int256 LIQUIDITY_DELTA = 10 ether;
 
     constructor(
         IPoolManager _poolManager,
@@ -37,8 +41,51 @@ contract Dononymous is BaseHook, ERC1155 {
     }
 
     // Main functionality
-    function provideDonut() public {
+    function provideCrumble(
+        PoolKey calldata key,
+        address org,
+        int24 _tickLower,
+        int24 _tickUpper
+    ) public {
         // add liquidity
+        organizationShare[org]++;
+
+        // Setup the swapping parameters
+        IPoolManager.ModifyPositionParams
+            memory modifyPositionParams = IPoolManager.ModifyPositionParams({
+                tickLower: _tickLower,
+                tickUpper: _tickUpper,
+                liquidityDelta: LIQUIDITY_DELTA
+            });
+
+        BalanceDelta delta = abi.decode(
+            poolManager.lock(
+                abi.encodeCall(
+                    this._handleModifyPosition,
+                    (key, modifyPositionParams)
+                )
+            ),
+            (BalanceDelta)
+        );
+
+        int256 delta0 = delta.amount0();
+        int256 delta1 = delta.amount1();
+
+        if (delta0 > 0) {
+            IERC20(Currency.unwrap(key.currency0)).transfer(
+                address(poolManager),
+                uint256(delta0)
+            );
+            poolManager.settle(key.currency0);
+        }
+
+        if (delta1 > 0) {
+            IERC20(Currency.unwrap(key.currency1)).transfer(
+                address(poolManager),
+                uint256(delta1)
+            );
+            poolManager.settle(key.currency1);
+        }
     }
 
     // Hook
@@ -56,9 +103,9 @@ contract Dononymous is BaseHook, ERC1155 {
     // Helper
     function _handleModifyPosition(
         PoolKey calldata key,
-        IPoolManager.ModifyPositionParams memory params,
-        bytes calldata hookData
-    ) public {
+        IPoolManager.ModifyPositionParams memory params
+    ) public returns (BalanceDelta) {
         BalanceDelta delta = poolManager.modifyPosition(key, params, bytes(""));
+        return delta;
     }
 }
