82cf3b8b57477a19e66a8d698ab11811258386e7
ryanycw <ryan@ryanycw.dev>
2023-09-23 19:23:58 -0400

    build: implement on-chain privacy group through semaphore

--- packages/contracts/contracts/DononymousSemaphore.sol
+++ packages/contracts/contracts/DononymousSemaphore.sol
@@ -1,21 +1,22 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@semaphore-protocol/contracts/interfaces/ISemaphore.sol";
 
-abstract contract Semaphore {
+abstract contract DononymousSemaphore is Ownable {
     ISemaphore public semaphore;
     mapping(address => uint256) public organizationGroup;
 
-    constructor(address _semaphoreAddress) {
-        semaphore = ISemaphore(_semaphoreAddress);
+    constructor(address semaphoreAddress) {
+        semaphore = ISemaphore(semaphoreAddress);
         // Organization Group 0 will be ourselves
         semaphore.createGroup(0, 20, address(this));
     }
 
-    function initOrganization(address _organization, uint256 _groupId, uint256 _depth) external {
-        semaphore.createGroup(_groupId, _depth, address(this));
+    function initOrganization(address _organization, uint256 _groupId, uint256 _depth) external onlyOwner {
         organizationGroup[_organization] = _groupId;
+        semaphore.createGroup(_groupId, _depth, address(this));
     }
 
     function joinService(uint256 _groupId, uint256 _identityCommitment) external {
@@ -29,7 +30,7 @@ abstract contract Semaphore {
         uint256 _nullifierHash,
         uint256 _externalNullifier,
         uint256[8] calldata _proof
-    ) external {
+    ) public {
         semaphore.verifyProof(_groupId, _merkleTreeRoot, _feedback, _nullifierHash, _externalNullifier, _proof);
     }
 }
--- packages/contracts/test/mock/MockSemaphoreHook.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
-
-import "../../contracts/Semaphore.sol";
-
-contract MockSemaphoreHook is Semaphore {
-    constructor(address _semaphoreAddress) Semaphore(_semaphoreAddress) {}
-}
